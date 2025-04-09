852ee5479533afda426ebe57c75e31da0384654b
Mamadou N'Gatté <doumstyle@gmail.com>
2025-04-09 18:11:48 +0200

    initial configurations, add header and nav

---
+++ components/Header.tsx
@@ -0,0 +1,32 @@
+import { ModeToggle } from "./ui/light-dark-mode";
+
+export const Header = () => {
+  return (
+    <header className="border-b-2 border-b-red-400 dark:border-b-yellow-300">
+      <div className="flex items-center justify-between p-4 bg-gray-800">
+        <div className="flex items-center space-x-2">
+          <a href="/">
+            <img src="/logo.png" alt="logo FPA" className="h-20 w-20" />
+          </a>
+          <h1 className="text-2xl font-bold text-white">
+            Les films de Plein Air
+          </h1>
+        </div>
+        <div className="flex items-center space-x-4">
+          <nav className="flex space-x-4 text-white">
+            <a href="/" className="hover:text-gray-400">
+              Accueil
+            </a>
+            <a href="/films" className="hover:text-gray-400">
+              Films
+            </a>
+            <a href="/apropos" className="hover:text-gray-400">
+              A Propos
+            </a>
+          </nav>
+          <ModeToggle />
+        </div>
+      </div>
+    </header>
+  );
+};
